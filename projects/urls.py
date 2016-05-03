22405153f83963e001153ff00b30d4f410fd34c6
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 20:39:12 +0000

    redirect index page to user profile

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 app_name = 'projects'
 urlpatterns = [
-    url(r'^$', login_required(views.IndexView.as_view()), name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'users/(?P<pk>[0-9]+)/', views.UserShow.as_view(), name='user_show'),
     url(r'projects/(?P<pk>[0-9]+)/', views.ProjectShow.as_view(), name='project_show'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Project
 
-class IndexView(generic.TemplateView):
-    template_name = 'projects/index.html'
+def index(request):
+    return HttpResponseRedirect(reverse('projects:user_show', args=(request.user.id,)))
 
 class UserShow(generic.DetailView):
     model = User
