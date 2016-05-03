659b2f7e926bce777633fafb4fc6116637e95c99
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 05:47:54 +0000

    make login required for index page

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', login_required(views.IndexView.as_view()), name='index'),
 ]
 
