19c25bd0ff55cb9be8786ddb7a00036a6d567b7a
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 17:26:54 +0000

    extend user model with location, about, and age fields

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    LOCATIONS = (
+        ('AL','Alabama'),
+        ('AK','Alaska'),
+        ('AZ','Arizona'),
+        ('AR','Arkansas'),
+        ('CA','California'),
+        ('CO','Colorado'),
+        ('CT','Connecticut'),
+        ('DE','Delaware'),
+        ('DC','District of Columbia'),
+        ('FL','Florida'),
+        ('GA','Georgia'),
+        ('HI','Hawaii'),
+        ('ID','Idaho'),
+        ('IL','Illinois'),
+        ('IN','Indiana'),
+        ('IA','Iowa'),
+        ('KS','Kansas'),
+        ('KY','Kentucky'),
+        ('LA','Louisiana'),
+        ('ME','Maine'),
+        ('MD','Maryland'),
+        ('MA','Massachusetts'),
+        ('MI','Michigan'),
+        ('MN','Minnesota'),
+        ('MS','Mississippi'),
+        ('MO','Missouri'),
+        ('MT','Montana'),
+        ('NE','Nebraska'),
+        ('NV','Nevada'),
+        ('NH','New Hampshire'),
+        ('NJ','New Jersey'),
+        ('NM','New Mexico'),
+        ('NY','New York'),
+        ('NC','North Carolina'),
+        ('ND',' North Dakota'),
+        ('OH','Ohio'),
+        ('OK','Oklahoma'),
+        ('OR','Oregon'),
+        ('PA','Pennsylvania'),
+        ('RI','Rhode Island'),
+        ('SC','South Carolina'),
+        ('SD','South Dakota'),
+        ('TN','Tennessee'),
+        ('TX','Texas'),
+        ('UT','Utah'),
+        ('VT','Vermont'),
+        ('VA','Virginia'),
+        ('WA','Washington'),
+        ('WV','West Virginia'),
+        ('WI','Wisconsin'),
+        ('WY','Wyoming'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, choices=LOCATIONS, blank=True, null=True)
+    about = models.TextField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+
