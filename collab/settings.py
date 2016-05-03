4f9b788f28800f93108d9cd356b7bd24da1d3c5a
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 03:56:53 +0000

    update database settings

--- collab/settings.py
+++ collab/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'collab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'collab',
+        'USER': 'admin',
+        'PASSWORD': 'RpADpUdg6JgH9itpOnno',
+        'HOST': 'collab.cgfndqbit3yw.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
