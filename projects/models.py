900b3398fe25531002bab56b9e2804fe9e3f96bb
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 18:02:27 +0000

    set __str__() to title for project model

--- projects/models.py
+++ projects/models.py
@@ -66,4 +66,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     created = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.title
 
