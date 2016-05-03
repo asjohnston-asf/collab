9cab1c433d9374d07238df718e352acb4a6990eb
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 19:21:02 +0000

    add stub page to show user details

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.decorators import login_required
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', login_required(views.IndexView.as_view()), name='index'),
+    url(r'users/(?P<pk>[0-9]+)/', views.UserShow.as_view(), name='user_show'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'projects/index.html'
+
+class UserShow(generic.DetailView):
+    model = User
+    template_name = 'projects/user_show.html'
 
