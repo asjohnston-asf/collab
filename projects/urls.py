c275784cc781bc671e46a25078aee63eab9c244b
Andrew Johnston <asjohnston@alaska.edu>
2016-05-03 20:04:16 +0000

    add user and project links

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.decorators import login_required
 
-app_name = 'polls'
+app_name = 'projects'
 urlpatterns = [
     url(r'^$', login_required(views.IndexView.as_view()), name='index'),
     url(r'users/(?P<pk>[0-9]+)/', views.UserShow.as_view(), name='user_show'),
