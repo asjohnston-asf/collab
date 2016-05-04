3a4c2f34117c6c6a99192534b2c24adc576b4ced
Andrew Johnston <asjohnston@alaska.edu>
2016-05-04 03:27:55 +0000

    drop email, name fields from new user form

--- projects/forms.py
+++ projects/forms.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password')
+        fields = ('username', 'password')
 
